5796a4ecbe38b6f0322c4a83dd6dcf133d2bfe75
fhagno <fhagno@yahoo.com>
2026-03-14 10:14:56 +0100

    Refactoring

--- main.py
+++ main.py
@@ -30,21 +30,30 @@ def save():
     website = website_entry.get().strip().title()
     email = email_entry.get().strip()
     password = password_entry.get().strip()
+    new_data = {
+        website: {
+            "email": email,
+            "password": password,
+        }
+    }
     if len(website) == 0 or len(password) == 0:
         messagebox.showinfo(title="Oops", message="Please make sure you haven't left any fields empty.")
     else:
-        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email} "
-                                                              f"\nPassword: {password} \nIs it ok to save?")
-        if is_ok:
-            try:
-                with open("data.json", "r") as data_file:
-                    data = json.load(data_file)
-            except FileNotFoundError:
-                data = {}
-            data[website] = {"email": email, "password": password}
+        try:
+            with open("data.json", "r") as data_file:
+                # Reading old data
+                data = json.load(data_file)
+        except FileNotFoundError:
             with open("data.json", "w") as data_file:
-                data.update(data[website])
-                json.dump(data, data_file)
+                json.dump(new_data, data_file, indent=4)
+        else:
+            # Updating old data with new data
+            data.update(new_data)
+
+            with open("data.json", "w") as data_file:
+                # Saving updated data
+                json.dump(data, data_file, indent=4)
+        finally:
             website_entry.delete(0, END)
             password_entry.delete(0, END)
 
